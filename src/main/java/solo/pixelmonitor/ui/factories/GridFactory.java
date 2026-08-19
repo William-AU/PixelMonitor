a25173ad282c98cd45ee5b0097e34d5f804177c4
William <william@fledelius.com>
2026-08-19 16:04:48 +0200

    Updated GridFactory with method to create a simple centered GridPane

--- src/main/java/solo/pixelmonitor/ui/factories/GridFactory.java
+++ src/main/java/solo/pixelmonitor/ui/factories/GridFactory.java
@@ -1,7 +1,9 @@
 package solo.pixelmonitor.ui.factories;
 
 import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
 
 public class GridFactory {
@@ -10,5 +12,11 @@ public class GridFactory {
         col.setHgrow(Priority.ALWAYS);
         col.setHalignment(HPos.CENTER);
         return col;
+    }
+
+    public static GridPane getCenteredGrid() {
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        return grid;
     }
 }
