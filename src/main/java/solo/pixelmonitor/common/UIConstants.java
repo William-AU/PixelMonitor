5c7b296c14ef39853fa3693fa72b7c94ad68034b
William <william@fledelius.com>
2026-08-19 15:54:50 +0200

    Added UI Constants for new dialogue windows

--- src/main/java/solo/pixelmonitor/common/UIConstants.java
+++ src/main/java/solo/pixelmonitor/common/UIConstants.java
@@ -9,4 +9,9 @@ public class UIConstants {
     public static final Insets DEFAULT_UNIFORM_INSETS = new Insets(10);
     public static final Insets DEFAULT_HORIZONTAL_INSETS = new Insets(0, 5, 0, 5);
 
+    public static final int NEW_DIALOGUE_DEFAULT_WIDTH = 1000;
+    public static final int NEW_DIALOGUE_DEFAULT_HEIGHT = 600;
+
+    public static final int NEW_DIALOGUE_SMALL_WIDTH = 400;
+    public static final int NEW_DIALOGUE_SMALL_HEIGHT = 200;
 }
